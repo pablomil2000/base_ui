47b042994fc6cdb25e44359bbafcdb435071aede
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2022-05-30 09:56:55 +0200

    css fix

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -36,7 +36,7 @@
         media="all">
 
     <!-- Main CSS-->
-    <link href="{{ asset('') }}CoolAdmin/css/theme.css" rel="stylesheet" media="all">
+    <link href="{{ asset('CoolAdmin/css/theme.css') }}" rel="stylesheet" media="all">
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
